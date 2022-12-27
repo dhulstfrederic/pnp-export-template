875747b25eed594b4f80080f1971f4e05fe26be8
dhulstfrederic <71829191+dhulstfrederic@users.noreply.github.com>
2022-12-27 16:01:58 +0100

    Add files via upload

---
+++ export-template.ps1
@@ -0,0 +1,8 @@
+﻿$url= "https://fredericdhulst.sharepoint.com/sites/HR-department"
+
+Connect-PnpOnline -Url $url -Interactive
+
+#Get-PnPSiteTemplate -Out "f:\codes\pnp\site-template-project.xml"
+Get-PnPSiteTemplate -Configuration "f:\codes\pnp\config.json" -Out "f:\codes\pnp\site-template-project.xml"
+
+Disconnect-PnPOnline
